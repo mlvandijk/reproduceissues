e4192fcde35506b336feb94e8bea93151a7cbcea
maritvandijk <mlvandijk@gmail.com>
2018-04-08 06:39:28 +0200

    #1341 Multiple annotations support for Cucumber-JVM

--- src/test/java/annotations/StepDefs.java
+++ src/test/java/annotations/StepDefs.java
@@ -1,25 +1,16 @@
 package annotations;
 
 import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 
 public class StepDefs {
 
     @Given("^the response body tag (.*) contains the following values:$")
-    public void the_response_body_tag_contains_the_following_values(String tag) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
-    }
-
-    @Then("^the response body attribute (.*) contains the following values:$")
-    public void the_response_body_attribute_contains_the_following_values(String attribute) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
-    }
-
+    @And("^the response body attribute (.*) contains the following values:$")
     @Then("^the response body XPath (.*) contains the following values:$")
-    public void the_response_body_XPath_contains_the_following_values(String xpath) {
+    public void the_response_body_contains_the_following_values(String xpath) {
         // Write code here that turns the phrase above into concrete actions
         throw new PendingException();
     }
