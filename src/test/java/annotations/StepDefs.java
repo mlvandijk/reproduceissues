ec07848187b28c5dee0c803ed6907c392cd4f20b
maritvandijk <mlvandijk@gmail.com>
2018-04-08 07:05:43 +0200

    #1341 Multiple annotations support for Cucumber-JVM

--- src/test/java/annotations/StepDefs.java
+++ src/test/java/annotations/StepDefs.java
@@ -9,6 +9,7 @@ public class StepDefs {
 
     @Given("^the response body tag (.*) contains the following values:$")
     @And("^the response body attribute (.*) contains the following values:$")
+//    @Then("^the response body attribute (.*) contains the following values:$") // Compile error
     @Then("^the response body XPath (.*) contains the following values:$")
     public void the_response_body_contains_the_following_values(String xpath) {
         // Write code here that turns the phrase above into concrete actions
